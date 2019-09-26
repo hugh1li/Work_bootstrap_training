2ebad34ba199a03035a2eaeca38a157d34fbccb5
hugh1li <hughli.netl@gmail.com>
2019-09-26 10:45:35 -0400

    Update run_simulation.R

--- code/run_simulation.R
+++ code/run_simulation.R
@@ -5,5 +5,8 @@ library(cumstats)
 library(ggpubr)
 
 # import the function and the dataframe
+source('code/montecarlo_simulation.R')
+distf <- read_csv('data/test_eg.csv')
 
+#run the code..
 montecarlo_simulation(pollutant_col_id = 2, distf = distf)
